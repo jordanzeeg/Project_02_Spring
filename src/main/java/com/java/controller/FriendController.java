b9e0cf465a8201c96ee38b964b3f4dd1a6018142
poholo502 <poholo502@gmail.com>
2019-04-25 09:13:52 -0500

    @CrossOrigin was added to Friend Controller

--- src/main/java/com/java/controller/FriendController.java
+++ src/main/java/com/java/controller/FriendController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -23,6 +24,7 @@ import com.java.service.FriendService;
 
 @RestController
 @RequestMapping("/friends")
+@CrossOrigin
 public class FriendController {
 	@Autowired
 	FriendService service;

a6c91d2ac827dbe186c5fe01123bfc976570a7c1
Mathew Schweigardt <schweigardt96@gmail.com>
2019-04-25 10:18:18 -0500

    Smol changes

--- src/main/java/com/java/controller/FriendController.java
+++ src/main/java/com/java/controller/FriendController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.java.service.FriendService;
 
 @RestController
 @RequestMapping("/friends")
+@CrossOrigin(origins = "http://localhost:4200")
 public class FriendController {
 	@Autowired
 	FriendService service;
@@ -143,4 +145,3 @@ public class FriendController {
 
 	}
 }
-

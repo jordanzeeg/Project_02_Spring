51c2b03d5ec9f7cc12fe633812812902b5fd7db0
Dylan Williamson <dylanwithgravity@gmail.com>
2019-04-25 21:25:43 -0500

    made changes to comment class

--- src/main/java/com/java/controller/CommentLikeController.java
+++ src/main/java/com/java/controller/CommentLikeController.java
@@ -1,41 +1,109 @@
 package com.java.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.java.dto.Friend;
+import com.java.dto.Messengering;
+import com.java.dto.Post;
+import com.java.dto.Comment;
 import com.java.dto.CommentLike;
 import com.java.service.CommentLikeService;
 import com.java.service.FriendService;
+import com.java.util.LoggerSingleton;
 
 @Controller
 @RequestMapping("/commentlikes")
 public class CommentLikeController {
-	@Autowired
+	@Autowired 
 	CommentLikeService service;
+	
+	@GetMapping
+	@ResponseBody
+	public ResponseEntity<?> getCommentLikes() {
+		List<CommentLike> commentLikes = service.getAll();
+		return ResponseEntity.ok(commentLikes);
+	}
+	@GetMapping("/byid{id}") // sets variable as part of the url
+	public ResponseEntity<?> getCommentLikeById(@PathVariable int id){ 
+		// @Pathvariable sets the variable in the url to the parameter
+		CommentLike commentLike = new CommentLike();
+		commentLike = service.get(id);
 
-	//@GetMapping getCommentLikes
+		return ResponseEntity.ok(commentLike);
+			}
+		
+		
+	@GetMapping("/bycommentid{commentId}") // sets variable as part of the url
+	public ResponseEntity<?> getCommentLikeByCommentId(@PathVariable int commentId) { 
+		// @Pathvariable sets the variable in the url to the parameter
+		List<CommentLike> commentLikes = service.getCommentLikeBasedOnCommentId(commentId);
+		return ResponseEntity.ok(commentLikes);
+		}
 
-	//@GetMapping("/get/byid{id}") getCommentLikeById
+	
 
-	//@GetMapping("/get/bycommentid{commentId}") getCommentLikeByCommentId
+	@PostMapping
+	public ResponseEntity<?> SaveCommentLike(@RequestBody CommentLike t){ 
+		// assumption of a form of some kind
+		Messengering mess = new Messengering("CommentLike already exists in Database. unable to update");
+		Messengering success = new Messengering("Save successful");
 
-	//@PostMapping("/save") saveCommentLike
+		if(t.getId()== 0) {
+		service.save(t);
+		return ResponseEntity.ok(success);
+		}else return ResponseEntity.ok(mess);
+	}
 
-	//@PostMapping("/update") updateCommentLike
+	
+	@PutMapping
+	public ResponseEntity<?> UpdateCommentLike(@RequestBody CommentLike t) {
+		// assumption of a form of some kind
+		Messengering mess = new Messengering("CommentLike does not exist in Database. unable to update");
+		Messengering success = new Messengering("CommentLike found in Database. Update successful");
+		if (t.getId()== 0) {
+			return ResponseEntity.ok(mess);
+		}else {
+		service.update(t);
+			return ResponseEntity.ok(success);
+		}
+	}
+	
+	@PostMapping("/delete")
+	public ResponseEntity<?> DeleteCommentLike(@RequestBody CommentLike t) { 
+		// assumption of a form of some kind
+		Messengering mess = new Messengering("CommentLike does not exist in Database. unable to update");
+		Messengering success = new Messengering("CommentLike found in Database. Delete successful");
+		if (t.getId()== 0) {
+			return ResponseEntity.ok(mess);
+		}
+		service.delete(t);
+		return ResponseEntity.ok(success);
+		
+	}
 
-	//@PostMapping("/delete") deleteCommentLike
-}
+		//TODO don't throw exception
+		
+		
+		//TODO CRUD CommentLike
+		//TODO getcommentlikebyid
+		//TODO getcommentlikebycommentId
+	}
 
--- src/main/java/com/java/controller/FriendController.java
+++ src/main/java/com/java/controller/FriendController.java
@@ -1,125 +1,119 @@
+
 package com.java.controller;
 
 import java.io.IOException;
-
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.java.dto.Friend;
 import com.java.service.FriendService;
 
 @RestController
 @RequestMapping("/friends")
+@CrossOrigin(origins = "http://localhost:4200")
 public class FriendController {
 	@Autowired
 	FriendService service;
 
-	
 	@GetMapping
 	@ResponseBody
 	public ResponseEntity<?> getFriends() throws IOException {
-		
+
 		List<Friend> friends = service.getAll();
 
-		return ResponseEntity.ok(friends); //copied from video tutorial by b2 Tech
-		//return ResponseEntity.ok().body(responseString.toString()); 
+		return ResponseEntity.ok(friends); // copied from video tutorial by b2 Tech
+		// return ResponseEntity.ok().body(responseString.toString());
 	}
-	
-	@GetMapping("/byid{id}") // sets variable as part of the url
-	
-	public ResponseEntity<?> getFriendById(@PathVariable("id") int id) throws IOException { 
+
+	@GetMapping("/{id}") // sets variable as part of the url
+
+	public ResponseEntity<?> getFriendById(@PathVariable("id") int id) throws IOException {
 		// @Pathvariable sets the variable in the url to the parameter
 
 		Friend friend = service.get(id);
 		if (friend == null) {
-			return ResponseEntity.ok().body("a friend with id: " + id+ " is not currently in database.");
+			return ResponseEntity.ok().body("a friend with id: " + id + " is not currently in database.");
 		} else {
 			return ResponseEntity.ok().body(friend.toString());
 		}
-		}
-	@GetMapping("/byusername{username}") // sets variable as part of the url
-	public ResponseEntity<?> getFriendByUsername(@PathVariable String username) { 
+	}
+
+	@GetMapping("/={username}") // sets variable as part of the url
+	public ResponseEntity<?> getFriendByUsername(@PathVariable String username) {
 		// @Pathvariable sets the variable in the url to the parameter
-		
-		Friend friend = service.getByUsername(username);
-		if (friend.getUsername() == null) {
-			return ResponseEntity.ok().body("a friend with username: " + username+ " is not currently in database.");
-		} else {
-			return ResponseEntity.ok().body(friend);
-		}
-		
+		return ResponseEntity.ok().body(service.getByUsername(username));
 
 		// TODO CRUD FRIENDS
 		// TODO getfriendbyname
 		// TODO getFriendsbyPostId
 	}
+
 //	@GetMapping("/getbypostid") //will add if we have time
 //	public void getFriendByPostId(@PathVariable int postId, HttpServletResponse response) throws IOException{
 //			
 //	}
 	@PostMapping()
-	public ResponseEntity<?> SaveFriend(@RequestBody Friend friend) { 
+	public ResponseEntity<?> SaveFriend(@RequestBody Friend friend) {
 		Friend dataFriend = service.getByUsername(friend.getUsername());
-		if(dataFriend.getId()== 0) {
-		service.save(friend);
-		String username = friend.getUsername();
-		return ResponseEntity.ok().body("Friend saved with username = " + username + " id = " + friend.getId());
-	}
-		else return ResponseEntity.ok("Friend already in database." + dataFriend);
+		if (dataFriend.getId() == 0) {
+			service.save(friend);
+			String username = friend.getUsername();
+			return ResponseEntity.ok().body("Friend saved with username = " + username + " id = " + friend.getId());
+		} else
+			return ResponseEntity.ok("Friend already in database." + dataFriend);
 	}
 
-	
-	@PutMapping() //  "/byid{id}"
+	@PutMapping() // "/byid{id}"
 	public ResponseEntity<?> UpdateFriend(@RequestBody Friend friend) throws IOException {
-		//@PathVariable("id")  int id,
+		// @PathVariable("id") int id,
 //		if(friend.getId()!= id) {
 //			return ResponseEntity.ok("friend id does not match id from path.");
 //		}
-		Friend dataFriend = service.getByUsername(friend.getUsername()); 
-		 if(dataFriend.getId() == 0) { //TODO ask people if null is what get actually returns
-			//response.getWriter().println("friend is not currently in database. save friend first");
+		Friend dataFriend = service.getByUsername(friend.getUsername());
+		if (dataFriend.getId() == 0) { // TODO ask people if null is what get actually returns
+			// response.getWriter().println("friend is not currently in database. save
+			// friend first");
 			return ResponseEntity.ok().body("friend is not currently in database. save friend first");
-		}else
-		{
-		service.update(friend);
-		//response.getWriter().println("Inserted successfully");
-		return ResponseEntity.ok().body("Inserted Successfully");
+		} else {
+			service.update(friend);
+			// response.getWriter().println("Inserted successfully");
+			return ResponseEntity.ok().body("Inserted Successfully");
 		}
 	}
-	@DeleteMapping("/byid{id}")
-	public ResponseEntity<?> DeleteFriend(@PathVariable("id") int id) { 
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> DeleteFriend(@PathVariable("id") int id) {
 		// assumption of a form of some kind
 		Friend friend = service.get(id);
-		if(friend == null) { //TODO ask people if null is what get actually returns
+		if (friend == null) { // TODO ask people if null is what get actually returns
 			return ResponseEntity.ok().body("friend not found to delete. Delete unsuccessful");
-		}else
-		{
-		service.delete(friend);
-		return ResponseEntity.ok().body("Deleted successfully. They weren't really our friend anyway");
+		} else {
+			service.delete(friend);
+			return ResponseEntity.ok().body("Deleted successfully. They weren't really our friend anyway");
 		}
 	}
-	
+
 	/*
 	 * 
-	 *  
-	 *  
-	 *  
-	 *  
-	 *  
-	 *  Testing Testing by Poho */
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * Testing Testing by Poho
+	 */
 	@PostMapping("/register")
 	public ResponseEntity<?> registerFriend(@RequestBody Friend friend) {
 		Friend dataFriend = service.getUsername(friend.getUsername());
@@ -152,4 +146,3 @@ public class FriendController {
 
 	}
 }
-
